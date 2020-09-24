6044f3d35c65672d92ce30eb26162041fb73d7d9
bartekmzyk <bartlomiej.mzyk@stazysta.comarch.pl>
2020-09-24 13:25:57 +0200

    author controller added

--- src/main/java/com/bookshop/controller/AuthorController.java
+++ src/main/java/com/bookshop/controller/AuthorController.java
@@ -1,9 +1,46 @@
 package com.bookshop.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bookshop.entity.Author;
+import com.bookshop.service.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/authors")
 public class AuthorController {
+
+    AuthorService authorService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        return ResponseEntity.ok().body(authorService.getAllAuthors());
+    }
+
+    @GetMapping("/author/{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable Long id) {
+        Optional<Author> author = authorService.getAuthorById(id);
+        return author.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Void> addAuthor(@RequestBody Author author) {
+        authorService.saveAuthor(author);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/delete")
+    public ResponseEntity<Void> updateAuthor(@RequestBody Author author) {
+        authorService.updateAuthor(author);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
+        authorService.deleteAuthor(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
